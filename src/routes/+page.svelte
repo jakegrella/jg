5fb4542cdef2352d6094593cd1b403fe36982319
jake grella <jake@jakegrella.com>
2022-10-03 20:04:51 -0500

    wip: initial structure and content example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,186 @@
+<header>
+	<h1>Jake Grella</h1>
+	<button>Contact</button>
+</header>
+<div class="complete-content">
+	<nav>
+		<img
+			src="https://images.unsplash.com/profile-1597117444375-4f06a9b0a4ccimage?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff"
+			alt="Jake Grella"
+		/>
+		<div>
+			<h3>Experience</h3>
+			<ul>
+				<li><a href="?">Northwestern Mutual</a></li>
+				<li><a href="?">Contractor</a></li>
+			</ul>
+		</div>
+		<div>
+			<h3>Projects</h3>
+			<ul>
+				<li><a href="?">Cloud Computing Jobs</a></li>
+				<li><a href="?">Personal Site</a></li>
+			</ul>
+		</div>
+		<div>
+			<h3>About</h3>
+			<p>
+				Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem
+				ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+			</p>
+		</div>
+	</nav>
+	<main>
+		<div class="main-header">
+			<div class="main-header-group">
+				<h3>Company</h3>
+				<p>Northwestern Mutual</p>
+			</div>
+			<div class="main-header-group">
+				<h3>When</h3>
+				<p>July 2021 - Current</p>
+			</div>
+		</div>
+		<div class="main-content">
+			<div class="main-content-content-group">
+				<div>
+					<h3>Summary</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit vulputate
+						felis, ac accumsan tellus posuere sit amet. Phasellus varius suscipit ullamcorper.
+						Aliquam erat volutpat. Pellentesque vel eros nec ante condimentum rutrum vel sit amet
+						diam. Sed erat elit, fringilla ut magna non, ultricies iaculis nisi. Donec et tellus
+						tristique ligula condimentum tincidunt ac at odio. Duis pulvinar dictum sollicitudin.
+						Praesent eget nisi in elit tempor egestas. Nam et orci rutrum, volutpat arcu nec,
+						posuere ipsum. Duis luctus nibh quis nulla euismod rutrum. Maecenas volutpat felis
+						lectus, sit amet tincidunt mauris bibendum vulputate.
+					</p>
+				</div>
+				<div>
+					<h3>Journey</h3>
+					<ul>
+						<li>
+							<div class="journey-info">
+								<p>Software Engineer</p>
+								<p>July 2022 - Current</p>
+							</div>
+						</li>
+						<li>
+							<div class="journey-info">
+								<p>Associate Software Engineer</p>
+								<p>July 2021 - July 2022</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="main-content-image-group">
+				<img
+					src="https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+					alt=""
+					class="main-content-image"
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+					alt=""
+					class="main-content-image"
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+					alt=""
+					class="main-content-image"
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1604079628040-94301bb21b91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+					alt=""
+					class="main-content-image"
+				/>
+			</div>
+		</div>
+	</main>
+</div>
+<footer>
+	<nav class="footer-nav">
+		<ul>
+			<li><a href="github.com/jakegrella">GitHub</a></li>
+			<li><a href="github.com/jakegrella">LinkedIn</a></li>
+			<li><a href="github.com/jakegrella">Twitter</a></li>
+		</ul>
+	</nav>
+	<img
+		src="https://icons.iconarchive.com/icons/wikipedia/flags/256/US-United-States-Flag-icon.png"
+		alt="American flag"
+	/>
+</footer>
+
+<style>
+	header {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.complete-content {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+
+	.main-header {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+
+	.main-header-group {
+		margin-right: 2rem;
+	}
+
+	.main-content {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+
+	.main-content-content-group {
+		flex-shrink: 2;
+	}
+
+	.main-content-image-group {
+		display: flex;
+		flex-flow: row wrap;
+	}
+
+	.main-content-image {
+		height: 10rem;
+		width: 15rem;
+		object-fit: cover;
+	}
+
+	.journey-info {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+	}
+
+	footer {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	footer img {
+		height: 2rem;
+		width: 2rem;
+		object-fit: contain;
+	}
+
+	.footer-nav ul {
+		list-style-type: none;
+		text-decoration: none;
+		padding: 0;
+	}
+
+	.footer-nav ul > li {
+		display: inline-block;
+	}
+</style>
