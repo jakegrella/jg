571bf0b8c8db451fe28c975b22df15a274791877
Jake Grella <jake@jakegrella.com>
2025-01-26 09:37:00 -0800

    chore: add contact page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} antialiased mx-4`}>
         <main>{children}</main>
-        <footer className="flex items-baseline gap-4 my-8">
+        <footer className="flex items-baseline gap-8 my-8">
           <span>© {new Date().getFullYear()} Jake Grella</span>
           <nav>
-            <ul>
+            <ul className="flex gap-2">
               <li>
                 <Link href="/privacy">Privacy</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
               </li>
             </ul>
           </nav>
