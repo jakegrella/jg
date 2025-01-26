8f1f6e8e636628fe2ad714f95a3f11b5a594f4f5
Jake Grella <jake@jakegrella.com>
2025-01-26 10:20:50 -0800

    chore: add bubsy - grid planner ap page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
 import Header from "./Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full">
       <Header landing />
+      <Link href="/tools/bubsy/grid-planner">
+        <div className="bg-zinc-600 px-6 py-4 rounded-lg w-fit flex flex-col gap-2 hover:bg-zinc-500">
+          <h2>Bubsy - Grid Planner</h2>
+          <Image
+            src="/grid-planner-screenshot.jpg"
+            alt="screenshot of Bubsy - Grid Planner iOS app"
+            width={100}
+            height={100}
+            className="rounded-sm"
+          />
+        </div>
+      </Link>
     </div>
   );
 }
---
+++ app/tools/bubsy/grid-planner/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function GridPlanner() {
+  return (
+    <div className="flex flex-col gap-4 max-w-3xl items-center my-0 mx-auto">
+      <Image
+        src={"/grid-planner-screenshot.jpg"}
+        alt="screenshot of Bubsy - Grid Planner iOS app"
+        width={200}
+        height={200}
+        className="rounded-sm"
+      />
+      <Image
+        src={"/apple-app-store-download.svg"}
+        alt="Download on the App Store (Apple)"
+        width={100}
+        height={100}
+      />
+      <p>
+        Use Bubsy - Grid Planner on iOS devices to plan your Instagram profile
+        grid. Unlike many of the other grid planning apps, Bubsy - Grid Planner
+        was made to be extremely lightweight and simple. All of the photos you
+        add to the app are linked to your device photos app, meaning we do not
+        store them anywhere else. We also do not require you to log into our app
+        using your Instagram account, or at all. Everything stays with you.
+      </p>
+    </div>
+  );
+}
