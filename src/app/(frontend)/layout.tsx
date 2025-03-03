10e4153196ee6c920e446a2de0e6ad6f3496c3fa
Jake Grella <jake@jakegrella.com>
2025-03-03 14:13:50 -0800

    chore: add vercel analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
 import './globals.css';
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
