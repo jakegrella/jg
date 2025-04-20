9dea80eb6ffbcdec162579da168fe9954d592ad9
Jake Grella <jake@jakegrella.com>
2025-04-20 08:36:50 -0700

    fix: typo, favicon file location, remove middleware

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="flex flex-col gap-4 max-w-3xl my-0 mx-auto">
       <p>
-        Please reach out to jake@jakegrella.com for any questions, comments, concers, projects or
+        Please reach out to jake@jakegrella.com for any questions, comments, concerns, projects or
         features to build, things to fix, etc.
       </p>
       <p>I will do everything I can to respond within 48 hours.</p>
