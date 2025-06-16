7d8939a74a33627fc2c52af60eb89c88818b621f
Jake Grella <jake@jakegrella.com>
2025-06-16 16:46:13 -0500

    chore: add error on blog page

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -3,26 +3,31 @@ import Link from 'next/link';
 import { getPayload } from 'payload';
 
 export default async function BlogPage() {
-  const payload = await getPayload({ config });
+  try {
+    const payload = await getPayload({ config });
 
-  const blogPosts = await payload.find({
-    collection: 'blog-posts',
-    where: {
-      publishedStatus: {
-        equals: 'published',
+    const blogPosts = await payload.find({
+      collection: 'blog-posts',
+      where: {
+        publishedStatus: {
+          equals: 'published',
+        },
       },
-    },
-    depth: 1,
-  });
+      depth: 1,
+    });
 
-  return (
-    <main className="flex flex-col h-full grow gap-8">
-      {blogPosts.docs.map((post) => (
-        <Link href={`/blog/${post.slug}`} key={post.id}>
-          <h2>{post.title}</h2>
-          <h3>{post.subtitle}</h3>
-        </Link>
-      ))}
-    </main>
-  );
+    return (
+      <main className="flex flex-col h-full grow gap-8">
+        {blogPosts.docs.map((post) => (
+          <Link href={`/blog/${post.slug}`} key={post.id}>
+            <h2>{post.title}</h2>
+            <h3>{post.subtitle}</h3>
+          </Link>
+        ))}
+      </main>
+    );
+  } catch (error) {
+    console.error('Error fetching blog posts:', error);
+    return <p>Error loading blog posts.</p>;
+  }
 }
