7458bc7dee23c2a237968541581622b5f31aa01c
Jake Grella <jake@jakegrella.com>
2025-06-16 17:57:00 -0500

    chore: add serverURL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -23,6 +23,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
+  serverURL: "https://jakegrella.com",
   collections: [Users, Media, EmailSubscribers, Visits, BlogPosts],
   db: vercelPostgresAdapter({
     pool: {
